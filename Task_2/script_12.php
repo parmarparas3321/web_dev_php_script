5e43e660798d0f8b28d60f87fff6b311949c3975
parmarparas3321 <143238573+parmarparas3321@users.noreply.github.com>
2023-10-25 10:55:43 +0530

    Add files via upload

---
+++ Task_2/script_12.php
@@ -0,0 +1,9 @@
+<!-- Write a PHP script to concatenate two strings and display the combined result. -->
+<?php
+$string1 = "Paras ";
+$string2 = "Parmar";
+
+$combined = $string1 . $string2;
+
+echo $combined;
+?>
